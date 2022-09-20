70f18950028b417c869862a662ee55624432fce4
SaikrishnaCh <roddick.sai@gmail.com>
2022-09-20 11:57:47 +0530

    19 sept 2022

---
+++ src/com/dsalgo/tree/bst/CheckIfBTIsBSTOrNot.java
@@ -0,0 +1,20 @@
+package com.dsalgo.tree.bst;
+
+import com.dsalgo.tree.TreeNode;
+
+public class CheckIfBTIsBSTOrNot {
+
+    private boolean checkBST(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+
+        if (checkBST(node.left, min, node.val) && checkBST(node.right, node.val, max)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
